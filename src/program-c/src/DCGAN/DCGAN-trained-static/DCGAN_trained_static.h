1b0f5f31718902e1c319d93ddd6377c677fb2da8
Liam Norris <liamnorris1231853211@gmail.com>
2022-02-24 06:07:31 +0000

    Working DCGAN with 16x16

---
+++ src/program-c/src/DCGAN/DCGAN-trained-static/DCGAN_trained_static.h
@@ -0,0 +1,74 @@
+// Bundle API auto-generated header file. Do not edit!
+// Glow Tools version: 2022-02-15 (5241e3c8c) ()
+
+#ifndef _GLOW_BUNDLE_DCGAN_TRAINED_STATIC_H
+#define _GLOW_BUNDLE_DCGAN_TRAINED_STATIC_H
+
+// #include <stdint.h>
+
+// ---------------------------------------------------------------
+//                       Common definitions
+// ---------------------------------------------------------------
+#ifndef _GLOW_BUNDLE_COMMON_DEFS
+#define _GLOW_BUNDLE_COMMON_DEFS
+
+// Glow bundle error code for correct execution.
+#define GLOW_SUCCESS 0
+#endif
+
+// Memory alignment definition with given alignment size
+// for static allocation of memory.
+#define GLOW_MEM_ALIGN(size)  __attribute__((aligned(size)))
+
+// Macro function to get the absolute address of a
+// placeholder using the base address of the mutable
+// weight buffer and placeholder offset definition.
+#define GLOW_GET_ADDR(mutableBaseAddr, placeholderOff)  (((uint8_t*)(mutableBaseAddr)) + placeholderOff)
+
+// ---------------------------------------------------------------
+//                          Bundle API
+// ---------------------------------------------------------------
+// Model name: "DCGAN_trained_static"
+// Total data size: 1458432 (bytes)
+// Activations allocation efficiency: 1.0000
+// Placeholders:
+//
+//   Name: "A0"
+//   Type: float<1 x 100 x 1 x 1>
+//   Size: 100 (elements)
+//   Size: 400 (bytes)
+//   Offset: 0 (bytes)
+//
+//   Name: "A21"
+//   Type: float<1 x 3 x 16 x 16>
+//   Size: 768 (elements)
+//   Size: 3072 (bytes)
+//   Offset: 448 (bytes)
+//
+// NOTE: Placeholders are allocated within the "mutableWeight"
+// buffer and are identified using an offset relative to base.
+// ---------------------------------------------------------------
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Placeholder address offsets within mutable buffer (bytes).
+#define DCGAN_TRAINED_STATIC_A0   0
+#define DCGAN_TRAINED_STATIC_A21  448
+
+// Memory sizes (bytes).
+#define DCGAN_TRAINED_STATIC_CONSTANT_MEM_SIZE     1430336
+#define DCGAN_TRAINED_STATIC_MUTABLE_MEM_SIZE      3520
+#define DCGAN_TRAINED_STATIC_ACTIVATIONS_MEM_SIZE  24576
+
+// Memory alignment (bytes).
+#define DCGAN_TRAINED_STATIC_MEM_ALIGN  64
+
+// Bundle entry point (inference function). Returns 0
+// for correct execution or some error code otherwise.
+int DCGAN_trained_static(uint8_t *constantWeight, uint8_t *mutableWeight, uint8_t *activations);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- src/program-c/src/DCGAN/DCGAN.c
+++ src/program-c/src/DCGAN/DCGAN.c
@@ -2,12 +2,98 @@
  * @brief C-based ONNX BPF program
  */
 #include <solana_sdk.h>
-#include "bundles-DCGAN-trained-dynamic/DCGAN_trained_dynamic.h"
-#include "bundles-DCGAN-trained-static/DCGAN_trained_static.h"
+#include <string.h>
+#include "DCGAN-trained-dynamic/DCGAN_trained_dynamic.h"
+#include "DCGAN-trained-static/DCGAN_trained_static.h"
 
-GLOW_MEM_ALIGN(DCGAN_TRAINED_STATIC_MEM_ALIGN);
-static const uint8_t constantWeight[DCGAN_TRAINED_STATIC_CONSTANT_MEM_SIZE] = {
-  #include "bundles-DCGAN-trained-static/DCGAN_trained_static.weights.txt"
+
+GLOW_MEM_ALIGN(DCGAN_TRAINED_STATIC_MEM_ALIGN)
+const static uint8_t constantWeight[DCGAN_TRAINED_STATIC_CONSTANT_MEM_SIZE] = {
+  #include "DCGAN-trained-static/DCGAN_trained_static.weights.txt"
+};
+
+#define HEAP_START_ADDRESS_ (uint64_t)0x300000000
+#define HEAP_LENGTH_ (uint64_t)(32 * 1024)
+
+
+const SymbolTableEntry *getWeightVar(const BundleConfig *config, const char *name) {
+  for (unsigned i = 0, e = config->numSymbols; i < e; i++) {
+    if (!strncmp(config->symbolTable[i].name, name, strlen(name))) {
+      return &config->symbolTable[i];
+    }
+  }
+  return NULL;
+}
+
+const SymbolTableEntry *getMutableWeightVar(const BundleConfig *config, const char *name) {
+  const SymbolTableEntry *mutableWeightVar = getWeightVar(config, name);
+  if (!mutableWeightVar) {
+    sol_log("No mutableWeightVar found");
+    return NULL;
+  }
+  if (mutableWeightVar->kind == 0) {
+    sol_log("mutableWeightVar kind is immutable");
+    return NULL;
+  }
+  return mutableWeightVar;
+}
+
+struct BumpAllocator {
+  uint64_t start;
+  uint64_t size;
+};
+
+void *alignedAlloc(struct BumpAllocator *self, uint64_t size, uint64_t align) {
+  uint64_t *pos_ptr = (uint64_t *)self->start;
+
+  uint64_t pos = *pos_ptr;
+  if (pos == 0) {
+    // First time, set starting position
+    pos = self->start + self->size;
+  }
+  if (pos < size) {
+    pos = 0;
+  } else {
+    pos = pos - size;
+  }
+  pos &= ~(align - 1);
+  if (pos < self->start + sizeof(uint8_t)) {
+    return NULL;
+  }
+  *pos_ptr = pos;
+  return (void *)pos;
+}
+
+uint8_t *allocateMutableWeightVars(struct BumpAllocator *heap, const BundleConfig *config) {
+  sol_log("Aligning memory for mutable weights in heap");
+  uint8_t *weights = (uint8_t *)(alignedAlloc(heap, config->mutableWeightVarsMemSize, config->alignment));
+  return weights;
+}
+
+float *getInferenceResults(const BundleConfig *config, uint8_t *mutableWeightVars) {
+  const SymbolTableEntry *outputWeights = getMutableWeightVar(config, "A21");
+  float *results = (float *)(mutableWeightVars + outputWeights->offset);
+  return results;
+}
+
+uint8_t *initMutableWeightVars(struct BumpAllocator *heap, const BundleConfig *config) {
+  sol_log("Inside initMutableWeightVars");
+  uint8_t *mutableWeightVarsAddr = allocateMutableWeightVars(heap, config);
+  sol_log("Allocated mutableWeightVars");
+
+  // A0
+  float a0[100] = {-0.330496591178819,-0.1231000694883476,-0.7984483569105248,0.22884050979484838,0.19133013653187203,0.6125432633506432,-0.3062297294509822,1.0621468486456525,-0.13854478723182956,1.6253202990605455,-1.2921923024748088,-0.8245019447922518,-0.3484332092407531,0.2515842402200945,0.5050110273556433,0.5261650965955069,0.5292802029457101,2.373710768054056,-0.901953394106964,0.27267471388643527,-0.4640220056850321,-0.9992854092485265,1.1362781567449485,-1.4728383260529165,0.5024673230453545,2.063521704044521,-0.8757086405834694,1.7561532306877148,0.4269235137119685,-1.0104569750473213,-0.0924002249646279,0.8661966923912441,0.7393806661354905,-0.450125815871679,-2.0960913742988887,2.145051266883868,-0.8189645021746585,0.30959437452794786,-1.1778617897532098,-1.3491713225138504,-0.3865572440052873,0.16604380514491116,-0.9422587782876317,0.8142563971080369,0.37041475511845906,-1.1040355694676451,1.0393968917802179,-1.0619365436731334,1.5533090604076045,1.4828508049133635,0.5840048993456167,-1.5936262016079425,1.8515082284477808,-0.9004774098499381,0.0337090547279996,-0.8119526798818553,-0.13969882957560703,0.8312266461792213,0.6961731096910785,0.20312120136308248,-0.502502685327624,-1.5199178763306262,-1.5413632731392575,-0.39145961377460975,-0.4081975588457413,0.6789438149132425,-0.308714334175275,-0.2556060510726535,-0.18526892439565112,0.5677979044395169,-0.4599260372134303,0.44453309055381685,0.068890016694916,-0.6585068953825021,-0.49670542930208583,-0.24238306853560115,-0.9012605220918253,-0.46096146827340856,-1.3935301779016442,-1.3042804557379828,-0.042897411974992423,0.9020564121431454,-0.9870285664204828,-0.9821673581175556,-1.2642175428463416,-2.109012466629439,-0.48006113342366163,-1.607553613374988,0.04795717422446833,1.64664335954184,-1.4103020146316114,-1.4000530910369868,-1.7079762446725424,-0.5044828787389366,-1.0949316318236406,0.8465237980666261,-1.978311213767623,1.8166799276628214,1.2458177364149676,0.16308031000744463,};
+  sol_log("Initialized a0 floats array");
+  const SymbolTableEntry *inputA0Var = getMutableWeightVar(config, "A0");
+  sol_log("Retrieved a0 inputVar");
+  sol_memcpy(mutableWeightVarsAddr + inputA0Var->offset, &a0, sizeof(float) * 100);
+  sol_log("memcpyied arrays");
+
+  return mutableWeightVarsAddr;
+}
+
+uint8_t *initActivations(struct BumpAllocator *heap, BundleConfig *config) {
+  return (uint8_t *)(alignedAlloc(heap, config->activationsMemSize, config->alignment));
 }
 
 uint64_t exec_onnx(SolParameters *params) {
@@ -26,32 +112,33 @@ uint64_t exec_onnx(SolParameters *params) {
     return ERROR_INCORRECT_PROGRAM_ID;
   }
 
-  float A0[64] = {0.9682414579846843,0.4481339758611895,-0.21684779752066421,-0.6259036638240845,0.6928551155266521,-0.5680150315342275,-0.3630327413743314,1.3187900306194995,0.34875318070557354,0.37449335858901117,-0.6447175505591933,-0.06939972166681546,-0.9593643831450365,-1.7357827264821168,0.10807244274762555,1.1086462291000239,0.7758008006908281,-1.2396190387549701,1.9388724271308517,-0.30446445269284056,0.7892547450432318,-1.2400015150423314,0.6172704963984191,-0.9448116305908916,0.02175708058959569,-1.1009891458915817,-0.1586483528973825,2.083149625035324,0.543028028331908,-1.0795153533935757,1.4471179625642339,-0.8115908980457811,0.9321504681915636,-0.061751450484871594,0.9928048268564711,-0.8060383819498921,1.098663722073884,-1.0191686715469959,1.4191238884976003,-1.0795833691461323,-0.544084417957494,0.9094919859997544,0.3512557862381586,1.2998994080515027,0.3200853321391851,0.08074969356220645,-0.47485497005854205,0.4857738145706917,1.8594731358865562,-1.0935485933254523,-0.1568911219258466,1.4441761102955806,-0.2576019284776974,-0.9781976287123395,-1.1144833379949541,0.3975267940249539,0.02984528074299735,-1.2156450174306734,-0.8165624413450648,-0.30421897145297716,0.09813807426077088,-0.8524258525025421,0.5393741155853573,-0.30866471418767216};
-
-  sol_log("Generating");
-  int errCode = DCGAN_trained_dynamic((uint8_t *) constantWeight, mutableWeight, activations);
-  
-  if (errCode != GLOW_SUCCESS) {
-    sol_log("Glow Exception");
-  }
-
-  // The data must be large enough to hold an uint32_t value
-  // if (greeted_account->data_len < sizeof(uint32_t)) {
-  //   sol_log("Greeted account data length too small to hold uint32_t value");
-  //   return ERROR_INVALID_ACCOUNT_DATA;
-  // }
-
-  // Increment and store the number of times the account has been greeted
-  // uint32_t *num_greets = (uint32_t *)greeted_account->data;
-  // *num_greets += 1;
-
   sol_log("Hello!");
+
+  struct BumpAllocator heap = {HEAP_START_ADDRESS_, HEAP_LENGTH_};
+  uint8_t *mutableWeightVarsAddr = initMutableWeightVars(&heap, &DCGAN_trained_dynamic_config);
+  sol_log("Initiated Mutable Weights");
+  uint8_t *activationsAddr = initActivations(&heap, &DCGAN_trained_dynamic_config);
+  sol_log("Initiated Activations");
+  sol_log_compute_units();
+
+  int errCode = DCGAN_trained_dynamic((uint8_t *)&constantWeight, mutableWeightVarsAddr, activationsAddr);
+  sol_log("Executed DCGAN");
+  if (errCode != GLOW_SUCCESS) {
+    sol_log("Error running bundle: error code");
+  }
+  
+  float *a21 = getInferenceResults(&DCGAN_trained_dynamic_config, mutableWeightVarsAddr);
+  float a21_total = 0;
+  for (int i = 0; i < 16 * 16; i++) {
+    a21_total += a21[i];
+  }
+  sol_log_64(0, 0, 0, 0, (int)a21_total);
   
   return SUCCESS;
 }
 
 extern uint64_t entrypoint(const uint8_t *input) {
-  sol_log("add 2inputs 3D C program entrypoint");
+  sol_log("DCGAN C program entrypoint");
 
   SolAccountInfo accounts[1];
   SolParameters params = (SolParameters){.ka = accounts};
