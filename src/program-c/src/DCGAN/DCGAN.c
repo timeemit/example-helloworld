3bd1d2ad8f16de1118bdb7af265559e134865da6
Liam Norris <liamnorris1231853211@gmail.com>
2022-03-16 15:37:30 +0000

    Serde input/output

--- src/program-c/src/DCGAN/DCGAN.c
+++ src/program-c/src/DCGAN/DCGAN.c
@@ -63,7 +63,8 @@ const static uint8_t constantWeight[DCGAN_TRAINED_STATIC_CONSTANT_MEM_SIZE] = {
 #define HEAP_START_ADDRESS_ (uint64_t)0x300000000
 #define HEAP_LENGTH_ (uint64_t)(32 * 1024)
 
-#define NZ 1
+#define INPUT_LENGTH_ 10
+#define OUTPUT_LENGTH_ 192
 static const int IMG_SIZE = 8;
 static const char *INPUT_VAR = "A0";
 static const char *OUTPUT_VAR = "A12";
@@ -129,18 +130,15 @@ float *getInferenceResults(const BundleConfig *config, uint8_t *mutableWeightVar
   return results;
 }
 
-uint8_t *initMutableWeightVars(struct BumpAllocator *heap, const BundleConfig *config) {
-  sol_log("Inside initMutableWeightVars");
+uint8_t *initMutableWeightVars(struct BumpAllocator *heap, const BundleConfig *config, float *paramData) {
   uint8_t *mutableWeightVarsAddr = allocateMutableWeightVars(heap, config);
   sol_log("Allocated mutableWeightVars");
 
-  // A0 to somehow be computed dynamically or passed in from client
-  float a0[NZ] = {0.47423121693043396,-0.5282680746784354,0.32941354491598535,0.8367122231652785,-0.9242990564027492,1.034541210871827,-1.3287637456932053,-0.41286506301773557,-0.09930055704305152,-0.013535512771139147,0.9553513527217113,0.05656428035571681,-0.36961798896662623,-0.027792667177632,1.5054505013132415,0.874266708141471,-0.8390414336155666,-0.98005548415165,0.6125328951111776,0.04891161400787087,0.5509755307746311,0.10761513330983134,0.1716293020433471,0.5920849565300466,0.14841621669549057,1.254210042600469,-0.3097030008239946,0.12622657155451214,-0.04191966271520088,0.9111061659699424,0.30841995722782156,0.09941792989307767,0.36701637160155554,-0.9348107950145716,-1.633922840448225,-1.024198976626713,1.0162380216217874,0.056417135986911275,0.979220166240875,-1.4611572977220904,1.0695556824495032,-1.1134468334133132,-0.05143702181003288,-1.8505955727315424,-0.11767996032387879,-0.3263432271774499,0.48315065605069574,0.5047781553059054,-0.8672224699703893,0.35264537905405263,0.8524724710093221,1.0021354808876095,-0.5382513178124323,-0.41645452549537326,0.2803919987764258,0.5372233499343916,-0.12937902659679293,-0.8550112233464158,-0.34291646334718895,0.5306289832589425,-0.019471483295322875,-0.4608088995925443,-0.5045502002322817,1.5956620820279201,-0.36358177887247156,-1.3270981022648056,-1.4161124144782749,0.4115769322290359,-1.4102690808657228,0.7245529137542382,-1.3946695326400453,-1.1008322524986809,1.300306151430027,-0.6081017588259483,-1.453615335295353,1.657788962868899,0.8492693074021435,-0.47499863433713146,0.7980171921422408,1.2100003914741897,-0.8563073701034595,-0.24149752651936698,0.4217386513885322,-0.24152538828018938,0.27352109588373613,0.8138689982095947,1.2152638544213743,-1.269303977369789,0.06558248832600873,0.15626193606349084,0.40544550925975403,1.1670524040056431,-0.7364212984521898,-0.43519686375709216,-0.30816363390091556,1.222397796189663,1.515498762364911,-0.6833775255503822,0.3592660130857487,-1.3153641497475044,};
-  sol_log("Initialized a0 floats array");
   const SymbolTableEntry *inputA0Var = getMutableWeightVar(config, INPUT_VAR);
   sol_log("Retrieved a0 inputVar");
-  sol_memcpy(mutableWeightVarsAddr + inputA0Var->offset, &a0, sizeof(float) * NZ);
-  sol_log("memcpyied arrays");
+
+  sol_memcpy(mutableWeightVarsAddr + inputA0Var->offset, paramData, sizeof(float) * INPUT_LENGTH_);
+  sol_log("Initalized a0 inputVar");
 
   return mutableWeightVarsAddr;
 }
@@ -152,51 +150,53 @@ uint8_t *initActivations(struct BumpAllocator *heap, BundleConfig *config) {
 uint64_t exec_onnx(SolParameters *params) {
 
   if (params->ka_num < 1) {
-    sol_log("Greeted account not included in the instruction");
+    sol_log("Program account not included in the instruction");
     return ERROR_NOT_ENOUGH_ACCOUNT_KEYS;
   }
 
   // Get the account to say hello to
-  SolAccountInfo *greeted_account = &params->ka[0];
+  SolAccountInfo *program_account = &params->ka[0];
 
   // The account must be owned by the program in order to modify its data
-  if (!SolPubkey_same(greeted_account->owner, params->program_id)) {
-    sol_log("Greeted account does not have the correct program id");
+  if (!SolPubkey_same(program_account->owner, params->program_id)) {
+    sol_log("Program account is not owned by this program");
     return ERROR_INCORRECT_PROGRAM_ID;
   }
 
   sol_log("Hello!");
 
-  // The data must be large enough to hold an uint32_t value
-  if (greeted_account->data_len < sizeof(uint32_t)) {
-    sol_log("Greeted account data length too small to hold uint32_t value");
-    return ERROR_INVALID_ACCOUNT_DATA;
+  // The program account's size must be large enough to hold the output
+  if (program_account->data_len < sizeof(float) * OUTPUT_LENGTH_) {
+    sol_log("Program account data length too small to hold the output");
+    return ERROR_ACCOUNT_DATA_TOO_SMALL;
   }
 
+  // Verify the input
+  if (params->data_len != sizeof(float) * INPUT_LENGTH_) {
+    sol_log("Program parameters is not precisely INPUT_LENGTH_ floats");
+    return ERROR_INVALID_INSTRUCTION_DATA;
+  }
+  
   struct BumpAllocator heap = {HEAP_START_ADDRESS_, HEAP_LENGTH_};
-  uint8_t *mutableWeightVarsAddr = initMutableWeightVars(&heap, &DCGAN_trained_dynamic_config);
+  uint8_t *mutableWeightVarsAddr = initMutableWeightVars(&heap, &DCGAN_trained_dynamic_config, (float *)params->data);
   sol_log("Initiated Mutable Weights");
+
   uint8_t *activationsAddr = initActivations(&heap, &DCGAN_trained_dynamic_config);
   sol_log("Initiated Activations");
-  sol_log_compute_units();
 
   int errCode = DCGAN_trained_dynamic((uint8_t *)&constantWeight, mutableWeightVarsAddr, activationsAddr);
   sol_log("Executed DCGAN");
+
   if (errCode != GLOW_SUCCESS) {
-    sol_log("Error running bundle: error code");
+    sol_log("Error running bundle");
+    return ERROR_CUSTOM_ZERO;
   }
   
   float *output = getInferenceResults(&DCGAN_trained_dynamic_config, mutableWeightVarsAddr);
-  float output_total = 0;
-  for (int i = 0; i < IMG_SIZE * IMG_SIZE; i++) {
-    output_total += output[i];
-  }
-
-  // Increment and store the vector total in the output
-  float *account_total = (float *)greeted_account->data;
-  *account_total = output_total;
+  sol_memcpy(program_account->data, output, sizeof(float) * OUTPUT_LENGTH_);
   
-  return ERROR_CUSTOM_ZERO;
+  sol_log_compute_units();
+  return SUCCESS;
 }
 
 extern uint64_t entrypoint(const uint8_t *input) {
